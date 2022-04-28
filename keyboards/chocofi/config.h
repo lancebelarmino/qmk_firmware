0a4c3401019d33e1b26d0ddcf7fd938485c5a72a
dtk <dtk@gmx.de>
2022-04-28 10:46:18 +0200

    Configure split setup

--- keyboards/chocofi/config.h
+++ keyboards/chocofi/config.h
@@ -6,6 +6,9 @@
 #define MATRIX_ROWS  8
 #define MATRIX_COLS  6
 
+#define USE_SERIAL
+#define SOFT_SERIAL_PIN D2
+
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
