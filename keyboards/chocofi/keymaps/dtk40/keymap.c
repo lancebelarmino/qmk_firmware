3d62d4e030a259add140ad913fe5e3ece4d4d545
dtk <dtk@gmx.de>
2022-04-28 15:03:27 +0200

    Configure a layer with symbols

--- keyboards/chocofi/keymaps/dtk40/keymap.c
+++ keyboards/chocofi/keymaps/dtk40/keymap.c
@@ -6,6 +6,7 @@ enum {
   COLEMK = 0,
   MOD_ARR,
   FN_NUM,
+  SYMBLS,
 
   NUM_LAYERS
 };
@@ -23,6 +24,7 @@ enum {
 //   Layer toggles
 #define KC_BSP_ LT(MOD_ARR, KC_BSPC)
 #define KC_ESC_ LT(FN_NUM, KC_ESC)
+#define KC_D_   LT(SYMBLS, KC_D)
 //  Right
 //   Home-row mods
 #define KC_N_ RSFT_T(KC_N)
@@ -32,6 +34,7 @@ enum {
 //   Layer toggles
 #define KC_SPC_ LT(MOD_ARR, KC_SPC)
 #define KC_ENT_ LT(FN_NUM, KC_ENT)
+#define KC_H_   LT(SYMBLS, KC_H)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -52,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [COLEMK] = LAYOUT_split_3x5_3(
         KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                               KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
         KC_A_,   KC_R_,   KC_S_,   KC_T_,   KC_G,                               KC_M,    KC_N_,   KC_E_,   KC_I_,   KC_O_,
-        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                               KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
+        KC_Z,    KC_X,    KC_C,    KC_D_,   KC_V,                               KC_K,    KC_H_,   KC_COMM, KC_DOT,  KC_SLSH,
                                    KC_LGUI, KC_BSP_, KC_ESC_,          KC_ENT_, KC_SPC_, KC_TAB
     ),
 
@@ -67,6 +70,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXX,    KC_F8,   KC_F7,   KC_F6,   KC_F5,                              KC_QUOT, KC_4,    KC_5,    KC_6,    KC_EQL,
         XXXX,    KC_F12,  KC_F11,  KC_F10,  KC_F9,                              KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS,
                                    XXXX,    XXXX,    XXXX,             KC_0,    KC_0,    KC_DOT
+    ),
+    [SYMBLS] = LAYOUT_split_3x5_3(
+        KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,                            KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
+        KC_EXLM, XXXX,    XXXX,    XXXX,    XXXX,                               KC_DQUO, XXXX,    KC_LBRC, KC_RBRC, KC_PLUS,
+        XXXX,    XXXX,    XXXX,    XXXX,    XXXX,                               KC_TILD, XXXX,    KC_LCBR, KC_RCBR, KC_PIPE,
+                                   XXXX,    XXXX,    XXXX,             XXXX,    XXXX,    XXXX
     )
     /*[<TMPL>] = LAYOUT_split_3x5_3(
      *   XXXX,    XXXX,    XXXX,    XXXX,    XXXX,                               XXXX,    XXXX,    XXXX,    XXXX,    XXXX,
