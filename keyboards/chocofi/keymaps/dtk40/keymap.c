4615e4bd87ee6f508d29f0ef699330130de8733d
dtk <dtk@gmx.de>
2022-04-28 14:40:21 +0200

    Configure layer with fn keys and numbers

--- keyboards/chocofi/keymaps/dtk40/keymap.c
+++ keyboards/chocofi/keymaps/dtk40/keymap.c
@@ -5,6 +5,7 @@
 enum {
   COLEMK = 0,
   MOD_ARR,
+  FN_NUM,
 
   NUM_LAYERS
 };
@@ -21,6 +22,7 @@ enum {
 #define KC_T_ LSFT_T(KC_T)
 //   Layer toggles
 #define KC_BSP_ LT(MOD_ARR, KC_BSPC)
+#define KC_ESC_ LT(FN_NUM, KC_ESC)
 //  Right
 //   Home-row mods
 #define KC_N_ RSFT_T(KC_N)
@@ -29,6 +31,7 @@ enum {
 #define KC_O_ LALT_T(KC_O)
 //   Layer toggles
 #define KC_SPC_ LT(MOD_ARR, KC_SPC)
+#define KC_ENT_ LT(FN_NUM, KC_ENT)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -50,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                               KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
         KC_A_,   KC_R_,   KC_S_,   KC_T_,   KC_G,                               KC_M,    KC_N_,   KC_E_,   KC_I_,   KC_O_,
         KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                               KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
-                                   KC_LGUI, KC_BSP_, KC_ESC,           KC_ENT,  KC_SPC_, KC_TAB
+                                   KC_LGUI, KC_BSP_, KC_ESC_,          KC_ENT_, KC_SPC_, KC_TAB
     ),
 
     [MOD_ARR] = LAYOUT_split_3x5_3(
@@ -58,6 +61,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXX,    XXXX,    XXXX,    KC_CAPS, XXXX,                               KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,XXXX,
         XXXX,    XXXX,    XXXX,    XXXX,    XXXX,                               XXXX,    XXXX,    XXXX,    XXXX,    XXXX,
                                    XXXX,    KC_DEL,  XXXX,             XXXX,    XXXX,    XXXX
+    ),
+    [FN_NUM] = LAYOUT_split_3x5_3(
+        XXXX,    KC_F4,   KC_F3,   KC_F2,   KC_F1,                              XXXX,    KC_7,    KC_8,    KC_9,    KC_MINS,
+        XXXX,    KC_F8,   KC_F7,   KC_F6,   KC_F5,                              KC_QUOT, KC_4,    KC_5,    KC_6,    KC_EQL,
+        XXXX,    KC_F12,  KC_F11,  KC_F10,  KC_F9,                              KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS,
+                                   XXXX,    XXXX,    XXXX,             KC_0,    KC_0,    KC_DOT
     )
     /*[<TMPL>] = LAYOUT_split_3x5_3(
      *   XXXX,    XXXX,    XXXX,    XXXX,    XXXX,                               XXXX,    XXXX,    XXXX,    XXXX,    XXXX,
