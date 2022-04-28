a2ceace69bb76c909401ec66250eaa34863603e3
dtk <dtk@gmx.de>
2022-04-28 12:48:35 +0200

    Configure home row mods

--- keyboards/chocofi/keymaps/dtk40/keymap.c
+++ keyboards/chocofi/keymaps/dtk40/keymap.c
@@ -1,6 +1,21 @@
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
+// Custom keycodes
+//  Left
+//   Home-row mods
+#define KC_A_ LALT_T(KC_A)
+#define KC_R_ LGUI_T(KC_R)
+#define KC_S_ LCTL_T(KC_S)
+#define KC_T_ LSFT_T(KC_T)
+//  Right
+//   Home-row mods
+#define KC_N_ RSFT_T(KC_N)
+#define KC_E_ RCTL_T(KC_E)
+#define KC_I_ RGUI_T(KC_I)
+#define KC_O_ LALT_T(KC_O)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
@@ -18,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       */
     [0] = LAYOUT_split_3x5_3(
         KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                               KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
-        KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                               KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
+        KC_A_,   KC_R_,   KC_S_,   KC_T_,   KC_G,                               KC_M,    KC_N_,   KC_E_,   KC_I_,   KC_O_,
         KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                               KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
                                    KC_LGUI, KC_BSPC, KC_ESC,           KC_ENT,  KC_SPC,  KC_TAB
     )
