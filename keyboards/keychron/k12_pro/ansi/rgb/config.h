778a30aacca11d4ffdf26673c9ed5d8fc4554c9f
Lance <lancebelarmino@gmail.com>
2024-08-25 10:50:04 +0800

    Add keychron layout

---
+++ keyboards/keychron/k12_pro/ansi/rgb/config.h
@@ -0,0 +1,68 @@
+/* Copyright 2023 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifdef RGB_MATRIX_ENABLE
+/* RGB Matrix driver configuration */
+#    define DRIVER_COUNT 1
+#    define DRIVER_ADDR_1 0b1110100
+#    define RGB_MATRIX_LED_COUNT 61
+
+/* Set to infinit, which is use in USB mode by default */
+#    define RGB_MATRIX_TIMEOUT RGB_MATRIX_TIMEOUT_INFINITE
+
+/* Allow shutdown of led driver to save power */
+#    define RGB_MATRIX_DRIVER_SHUTDOWN_ENABLE
+
+/* Turn off backlight on low brightness to save power */
+#    define RGB_MATRIX_BRIGHTNESS_TURN_OFF_VAL 48
+
+/* Indication led */
+#    define DIM_CAPS_LOCK
+#    define CAPS_LOCK_INDEX 28
+#    define LOW_BAT_IND_INDEX 56
+
+// RGB Matrix Animation modes. Explicitly enabled
+// For full list of effects, see:
+// https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
+#    define RGB_MATRIX_KEYPRESSES
+#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+
+/* Set LED driver current */
+#    define CKLED2001_CURRENT_TUNE \
+        { 0x50, 0x50, 0x50, 0x50, 0x50, 0x50, 0x50, 0x50, 0x50, 0x50, 0x50, 0x50 }
+#endif
+
+#define ONESHOT_TAP_TOGGLE 5
+#define ONESHOT_TIMEOUT 5000 
+#define TAPPING_TERM 300
+#define TAPPING_TERM_PER_KEY
+#define TAPPING_TOGGLE 1
+// #define MK_KINETIC_SPEED
+// #define MOUSEKEY_INITIAL_SPEED 760
+// #define MOUSEKEY_BASE_SPEED 2000
+// #define MOUSEKEY_DECELERATED_SPEED 450
+// #define MOUSEKEY_ACCELERATED_SPEED 2000
+// #define MOUSEKEY_MOVE_DELTA 4
+// #define MOUSEKEY_INTERVAL 6
+
+#define MK_3_SPEED
+#define MK_MOMENTARY_ACCEL
+#define MK_C_OFFSET_UNMOD 12
+#define MK_C_OFFSET_0 4
+#define MK_C_OFFSET_1 6
+#define MK_C_OFFSET_2 26
---
+++ keyboards/keychron/k12_pro/ansi/rgb/keymaps/lancebelarmino/keymap.c
@@ -0,0 +1,132 @@
+/* Copyright 2023 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+// clang-format off
+enum layers {
+  MAC_BASE,
+  WIN_BASE,
+  NAV,
+  MOUSE,
+  FUNC,
+  GAMING,
+  GAMING_FUNC,
+  MENU
+};
+
+enum tapdance_keycodes {
+    TD_CAPS,
+    TD_ESC,
+    TD_BSLS,
+    TD_DLR,
+};
+
+enum custom_keycodes {
+    MR_PS = SAFE_RANGE
+};
+
+enum {
+    OS_UNSURE,
+    OS_LINUX,
+    OS_WINDOWS,
+    OS_MACOS,
+    OS_IOS,
+} os_variant_t;
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS),
+    [TD_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_GRAVE, KC_ESC),
+    [TD_BSLS] = ACTION_TAP_DANCE_DOUBLE(KC_BSLS, KC_AT),
+    [TD_DLR] = ACTION_TAP_DANCE_DOUBLE(KC_DLR, KC_CIRC),
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // [WIN_BASE] = LAYOUT_61_ansi(
+    //     TD(TD_ESC),KC_1,    KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
+    //     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
+    //     TT(NAV),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     TT(SYM),  LT(MENU, KC_QUOT),  KC_ENT,
+    //     TD(TD_CAPS),        KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,
+    //     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 TO(GAMING),TO(NAV), TO(FUNC), TO(WIN_BASE)),
+
+    [MAC_BASE] = LAYOUT_61_ansi(
+        KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
+        TT(NAV),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            TD(TD_CAPS),
+        KC_LCTL,  KC_LOPTN, KC_LGUI,                                KC_SPC,                                 TO(GAMING),OSL(FUNC),TO(FUNC),TO(MAC_BASE)),
+
+    [WIN_BASE] = LAYOUT_61_ansi(
+        TD(TD_ESC),KC_1,    KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
+        TT(NAV),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  LT(MENU,  KC_QUOT), KC_ENT,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            TD(TD_CAPS),
+        KC_LCTL,  KC_LGUI, KC_LALT,                                KC_SPC,                                 TO(GAMING),OSL(FUNC),TO(FUNC),TO(WIN_BASE)),
+
+    [NAV] = LAYOUT_61_ansi(
+        KC_ESC,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        KC_TAB,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_UP,    _______,  _______,  _______,  _______,  _______,
+        TO(WIN_BASE),_______,_______, _______,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_RIGHT, _______,  _______,            KC_ENT,
+        KC_LSFT,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_RSFT,
+        KC_LCTL,  KC_LGUI,  KC_LALT,                                _______,                                TO(GAMING),OSL(FUNC), TO(FUNC),TO(WIN_BASE)),
+
+    [MENU] = LAYOUT_61_ansi(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        KC_TAB,   KC_F1,    KC_F3,    KC_F12,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
+
+    [FUNC] = LAYOUT_61_ansi(
+        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_MOD,
+        _______,  _______,  _______,  _______,  MR_PS,    _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,             RGB_TOG, RGB_VAI,  RGB_VAD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_SPI,  RGB_SPD,  RGB_MOD,            _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
+
+    [GAMING] = LAYOUT_61_ansi(
+        KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
+        KC_EQL,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,
+        KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 TO(GAMING),TO(FUNC), TO(FUNC),TO(WIN_BASE)),
+
+    [GAMING_FUNC] = LAYOUT_61_ansi(
+        TO(GAMING),KC_F1,   KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+      // Print Screen
+      case MR_PS:
+          if (record->event.pressed) {
+            register_code(KC_LCTL);
+            register_code(KC_RALT);
+            register_code(KC_PSCR);
+          } else {
+            unregister_code(KC_LCTL);
+            unregister_code(KC_RALT);
+            unregister_code(KC_PSCR);
+          }
+          break;
+    }
+    return true;
+};
+
