652eeacaf4cba8786b6beb49a8df3661407d1b31
Lance <lancebelarmino@gmail.com>
2025-02-02 17:31:07 +0800

    Make middle thumb lt

--- keyboards/chocofi/keymaps/lncblrmn/keymap.c
+++ keyboards/chocofi/keymaps/lncblrmn/keymap.c
@@ -2,10 +2,10 @@
 
 #include "oneshot.h"
 
-#define LR_NAV LT(NAV, KC_TAB)
+#define LR_NAV LT(NAV, KC_ESC)
 #define LR_SYM LT(SYM, KC_ESC)
-#define LR_MSE LT(MOUSE, QK_REP)
-#define LR_FUN LT(FUNC, QK_REP)
+#define LR_MSE TT(MOUSE)
+#define LR_FUN TT(FUNC)
 
 #define MT_SPC MT(MOD_LSFT, KC_SPC)
 #define MT_ENT MT(MOD_LCTL | MOD_LALT | MOD_LGUI, KC_ENT) 
@@ -75,33 +75,6 @@ enum {
     CO_MCOMM,
     CO_COMMDOT,
 };
-
-typedef enum {
-    TD_NONE,
-    TD_UNKNOWN,
-    TD_SINGLE_HOLD,
-    TD_DOUBLE_HOLD,
-} td_state_t;
-
-typedef struct {
-    bool is_press_action;
-    td_state_t state;
-} td_tap_t;
-
-// Tap dance enums
-enum {
-    TD_NAV_MOUSE,
-    TD_SYM_FUNC
-};
-
-td_state_t cur_dance(tap_dance_state_t *state);
-
-// For the x tap dance. Put it here so it can be used in any keymap
-void b_finished(tap_dance_state_t *state, void *user_data);
-void b_reset(tap_dance_state_t *state, void *user_data);
-
-void d_finished(tap_dance_state_t *state, void *user_data);
-void d_reset(tap_dance_state_t *state, void *user_data);
 
 // Keymap
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -180,8 +153,8 @@ combo_t key_combos[] = {
     [CO_WE] = COMBO(we_combo, MR_Z),
     [CO_ER] = COMBO(er_combo, MR_RZ),
     [CO_WR] = COMBO(wr_combo, MR_AT),
-    [CO_SD] = COMBO(sd_combo, KC_ESC),
-    [CO_DF] = COMBO(df_combo, KC_TAB),
+    [CO_SD] = COMBO(sd_combo, KC_TAB),
+    [CO_DF] = COMBO(df_combo, QK_REP),
     [CO_SF] = COMBO(sf_combo, KC_LGUI),
     [CO_XC] = COMBO(xc_combo, KC_ENT),
     [CO_CV] = COMBO(cv_combo, KC_BSPC),
@@ -202,8 +175,7 @@ oneshot_state os_cmd_state = os_up_unqueued;
 
 bool is_oneshot_cancel_key(uint16_t keycode) {
     switch (keycode) {
-    case LR_NAV:
-    case LR_SYM:
+    case KC_CLEAR:
         return true;
     default:
         return false;
@@ -409,6 +381,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
               alt_tab_timer = timer_read();
           }
           break;
+      case LR_NAV:
+          if (record->event.pressed && record->tap.count > 0) {
+                if (get_mods()) {
+                    update_oneshot(
+                        &os_shft_state, KC_LSFT, OS_SHFT,
+                        KC_CLEAR, record
+                    );
+                    update_oneshot(
+                        &os_ctrl_state, KC_LCTL, OS_CTRL,
+                        KC_CLEAR, record
+                    );
+                    update_oneshot(
+                        &os_alt_state, KC_LALT, OS_ALT,
+                        KC_CLEAR, record
+                    );
+                    update_oneshot(
+                        &os_cmd_state, KC_LCMD, OS_CMD,
+                        KC_CLEAR, record
+                    );
+                    return false;
+                }
+                return true;
+            }
+            return true;
     }
 
     return true;
@@ -429,69 +425,3 @@ void matrix_scan_user(void) {
     }
   }
 }
-
-// Tap Dance (Left to right button A -> F)
-td_state_t cur_dance(tap_dance_state_t *state) {
-    if (state->count == 1) {
-        // if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
-        if (state->pressed) return TD_SINGLE_HOLD;
-        else return TD_NONE;
-    } else if (state->count == 2) {
-        // if (state->interrupted || !state->pressed) return TD_DOUBLE_TAP;
-        if (state->pressed) return TD_DOUBLE_HOLD;
-        else return TD_NONE;
-    } else return TD_UNKNOWN;
-}
-
-// B - Left Thumb
-static td_tap_t btap_state = {
-    .is_press_action = true,
-    .state = TD_NONE
-};
-
-void b_finished(tap_dance_state_t *state, void *user_data) {
-    btap_state.state = cur_dance(state);
-    switch (btap_state.state) {
-        case TD_SINGLE_HOLD: layer_on(NAV); break;
-        case TD_DOUBLE_HOLD: layer_on(MOUSE);  break;
-        default: break;
-    }
-}
-
-void b_reset(tap_dance_state_t *state, void *user_data) {
-    switch (btap_state.state) {
-        case TD_SINGLE_HOLD: layer_off(NAV); break;
-        case TD_DOUBLE_HOLD: layer_off(MOUSE); break;
-        default: break;
-    }
-    btap_state.state = TD_NONE;
-}
-
-// D - Right Thumb
-static td_tap_t dtap_state = {
-    .is_press_action = true,
-    .state = TD_NONE
-};
-
-void d_finished(tap_dance_state_t *state, void *user_data) {
-    dtap_state.state = cur_dance(state);
-    switch (dtap_state.state) {
-        case TD_SINGLE_HOLD: layer_on(SYM); break;
-        case TD_DOUBLE_HOLD: layer_on(FUNC);  break;
-        default: break;
-    }
-}
-
-void d_reset(tap_dance_state_t *state, void *user_data) {
-    switch (dtap_state.state) {
-        case TD_SINGLE_HOLD: layer_off(SYM); break;
-        case TD_DOUBLE_HOLD: layer_off(FUNC); break;
-        default: break;
-    }
-    dtap_state.state = TD_NONE;
-}
-
-tap_dance_action_t tap_dance_actions[] = {
-    [TD_NAV_MOUSE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, b_finished, b_reset),
-    [TD_SYM_FUNC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, d_finished, d_reset)
-};
